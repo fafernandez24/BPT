8b77a7ad420db7a0df6e002983179b9736d3fbf9
Freddy Fernandez <fafernandez.24@est.ucab.edu.ve>
2025-05-27 12:03:05 -0400

    Se comenzo la clase hija de Jugador

---
+++ BPT/src/main/java/bpt/Player.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bpt;
+
+/**
+ *
+ * @author Freddy
+ */
+public class Player {
+    
+    // Atributtes
+    
+    protected int matchPlayed;
+    protected int matchesWon;
+    
+}
