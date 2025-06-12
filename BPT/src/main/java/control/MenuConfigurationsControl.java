927699a9d9aff999d582913150d4a55622f1694e
Karchito <carlosepintoa01@gmail.com>
2025-06-11 20:21:59 -0400

    se agrego MenuConfigurationsControl

---
+++ BPT/src/main/java/control/MenuConfigurationsControl.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+import javax.swing.JTextField;
+/**
+ *
+ * @author Asus
+ */
+public class MenuConfigurationsControl {
+    public static void  configurationFocusGained (JTextField dataEnter, String message){
+        if (dataEnter.getText().equals(message)) dataEnter.setText("");
+    }
+    
+    public static void configurationFocusLost(JTextField dataEnter, String message){
+        String data = dataEnter.getText();
+        if (data.trim().isEmpty()) dataEnter.setText(message);
+    }
+}
