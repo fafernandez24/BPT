c69ae927db5e62c931dedac8cd2f14f1f439fde2
Karchito <carlosepintoa01@gmail.com>
2025-06-30 23:53:36 -0400

    pequeñas modificaciones

--- BPT/src/main/java/controller/MenuAdministratorControl.java
+++ BPT/src/main/java/controller/MenuAdministratorControl.java
@@ -61,5 +61,9 @@ public class MenuAdministratorControl {
         DefaultTableModel defaultTable = (DefaultTableModel) table.getModel();
         defaultTable.setRowCount(0);
     }
+    
+    public static int totalPlayers(List<Player> players){
+        return players.size();
+    }
             
 }
