73a90b5b62eea20860ffb11b3429f125f17bd453
Calo <eduyeli958@gmail.com>
2025-07-02 17:56:00 -0400

    Add files via upload

--- BPT/src/main/java/controller/MenuConfigurationsControl.java
+++ BPT/src/main/java/controller/MenuConfigurationsControl.java
@@ -3,19 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controller;
+import static controller.TypeBecomeType.StringBecomeLocalDate;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import model.Group;
 import model.Match;
 import model.Player;
+import model.Tournament;
 /**
  *
  * @author Asus
  */
 public class MenuConfigurationsControl {
+    
+    private static Tournament activeTournament; 
+    
+    
+    public MenuConfigurationsControl(Tournament activeTournament) {
+        MenuConfigurationsControl.activeTournament = activeTournament; 
+    }
+    
     public static void  configurationFocusGained (JTextField dataEnter, String message){
         if (dataEnter.getText().equals(message)) dataEnter.setText("");
     }
@@ -24,143 +34,135 @@ public class MenuConfigurationsControl {
         String data = dataEnter.getText();
         if (data.trim().isEmpty()) dataEnter.setText(message);
     }
-    public static boolean deletePlayer(List<Player> playersPlay, JTextField name, JTextField cedula){
-        String nameDelete= name.getText();
-        String cedulaDelete= cedula.getText();
-        boolean playerDelete= false;
-        for (int i= playersPlay.size()-1; i>= 0 ;i--)  {
-            Player player= playersPlay.get(i); 
-            String namePlayer= player.getName();
-            String cedulaPlayer= player.getId();
-            if (nameDelete.equals(namePlayer) || cedulaDelete.equals(cedulaPlayer)) {
-                playersPlay.remove(i);
-                playerDelete= true;
-                break;
-            }
-        }
-        return playerDelete;
+ 
+    public static void deletePlayer(JTextField cedula){
+       try{
+           String cedulaDelete = cedula.getText();
+           //Si existen grupos:
+           if(activeTournament.getGroupsNumber() > 0) { //Primero lo borra de los grupos
+               List<Group> grup = activeTournament.getGroupsList();
+               for(Group g : grup) {
+                   List<Player> allPlayer = g.getPlayerList();
+                   for(int i = 0; i < allPlayer.size(); i++) {
+                       Player p = allPlayer.get(i);
+                       if(p.getId().equals(cedulaDelete)) {
+                           allPlayer.remove(i);
+                       }
+                   }
+               } //Luego lo borra del torneo en si
+           for (int i =0; i > activeTournament.getParticipantsList().size(); i++){
+               List<Player> playerTournament = activeTournament.getParticipantsList();
+               if (playerTournament.get(i).getId().equals(cedulaDelete)){
+                   playerTournament.remove(i);
+                   }
+               }           
+           }   
+           else {  //Si no hay grupos:
+               List<Player> allPlayersTournament = activeTournament.getParticipantsList();
+               for(int i = 0; i < allPlayersTournament.size(); i++) {
+                   Player p = allPlayersTournament.get(i);
+                   if(p.getId().equals(cedulaDelete)) {
+                       allPlayersTournament.remove(i);
+                   }
+               }
+           }
+       }
+       catch(Exception e) {
+           JOptionPane.showMessageDialog(null, "Error al guardar los cambios", "ERROR", JOptionPane.ERROR_MESSAGE);
+       } 
+   }
+    
+    public static void addPlayer(JTextField name, JTextField cedula, JTextField email, JTextField phone, JTextField team, JComboBox category, JTextField date) {
+       try{
+       Player newPlayer = new Player();
+       newPlayer.readPlayer(name, cedula, phone, email, category, team, date);
+       List<Player> players = activeTournament.getParticipantsList();
+       players.add(newPlayer);
+    } catch(Exception e) {
+           JOptionPane.showMessageDialog(null, "Error al guardar los cambios", "ERROR", JOptionPane.ERROR_MESSAGE);
+       }
     }
     
-    public static void addPlayer(List<Player> playersPlay, JTextField name, JTextField cedula, JTextField email, JTextField phone, JTextField equipo, JComboBox category) {
-        if(name.getText().isEmpty() || cedula.getText().isEmpty() || email.getText().isEmpty() || phone.getText().isEmpty() || equipo.getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Todos los campos deben estar llenos", "ERROR", JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-        Player newPlayer = new Player();
-        newPlayer.setName(name.getText());
-        newPlayer.setId(cedula.getText());
-        newPlayer.setEmail(email.getText());
-        newPlayer.setPhoneNumber(phone.getText());
-        newPlayer.setTeamName(equipo.getText());
-        newPlayer.setCategory((String) category.getSelectedItem());
-        playersPlay.add(newPlayer);
+    public static void editPlayer(JTextField cedulaLogin, JTextField name, JTextField email, JTextField phone, JTextField cedula, JTextField team ,JComboBox category, JTextField date) {
+       String cedulaLog = cedulaLogin.getText();
+       List<Player> listPlayers = activeTournament.getParticipantsList();
+       for(Player player : listPlayers) {
+           if(player.getId().equals(cedulaLog)) {
+               player.readPlayer(name, cedula, phone, email, category, team, date);
+               listPlayers.add(player);
+               return;
+           } else { JOptionPane.showMessageDialog(null, "No se puedo modificar el jugador", "ERROR", JOptionPane.ERROR_MESSAGE);}
+       }
     }
     
-    public static void editPlayer(List<Player> playersPlay, JTextField cedulaLogin, JTextField name, JTextField email, JTextField phone, JTextField cedula, JTextField team ,JComboBox category, JTextField date) {
-        if(name.getText().isEmpty() || cedula.getText().isEmpty() || email.getText().isEmpty() || phone.getText().isEmpty() || team.getText().isEmpty() || date.getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Todos los campos deben estar llenos", "ERROR", JOptionPane.ERROR_MESSAGE);
-            return;
+    public static void addMatch(List<Match> matchList, JTextField nameA, JTextField nameB, JTextField date){
+        String playerAname = nameA.getText();
+        String playerBname = nameB.getText();
+        LocalDate dateFormater = StringBecomeLocalDate(date.getText());
+        Player playerA = null;
+        Player playerB = null;
+        for(Player p : activeTournament.getParticipantsList()) {
+            if(p.getName().equals(playerAname)) { playerA = p;}
+            if(p.getName().equals(playerBname)) { playerB = p;}
         }
-        String cedulaLog = cedulaLogin.getText();
-        String namePlayer = name.getText();
-        String emailPlayer = email.getText();
-        String phonePlayer = phone.getText();
-        String cedulaPlayer = cedula.getText();
-        String teamPlayer = team.getText();
-        String categoryPlayer = (String) category.getSelectedItem();
-        String datePlayer = date.getText();
-        DateTimeFormatter dateFormatter= DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dateFormater= LocalDate.parse(datePlayer, dateFormatter);
-        
-        for(Player player : playersPlay) {
-            if(player.getId().equals(cedulaLog)) {
-                player.setName(namePlayer);
-                player.setEmail(emailPlayer);
-                player.setPhoneNumber(phonePlayer);
-                player.setId(cedulaPlayer);
-                player.setTeamName(teamPlayer);
-                player.setCategory(categoryPlayer);
-                player.setDateBirth(dateFormater);
-                return;
-            } else { JOptionPane.showMessageDialog(null, "No se puedo modificar el jugador", "ERROR", JOptionPane.ERROR_MESSAGE);}
+        if(playerA == null || playerB == null) {
+            JOptionPane.showMessageDialog(null, "Uno o dos jugadores no existe", "ERROR", JOptionPane.ERROR_MESSAGE);
         }
-    }
-    
-    public static void addMatch(List<Match> listMatch, JTextField nameA, JTextField nameB, JTextField date) {
-        String playerA = nameA.getText();
-        String playerB = nameB.getText();
-        String dateForm = date.getText();
-        if(playerA.isEmpty() || playerB.isEmpty() || dateForm.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Todos los campos deben estar llenos", "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        DateTimeFormatter dateFormatter= DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dateFormater= LocalDate.parse(dateForm, dateFormatter); 
-        Player player1 = new Player();
-        player1.setName(playerA);
-        Player player2 = new Player();
-        player2.setName(playerB);
-        
         Match newMatch = new Match();
-        newMatch.setPlayerA(player1);
-        newMatch.setPlayerB(player2);
+        newMatch.setPlayerA(playerA);
+        newMatch.setPlayerB(playerB);
         newMatch.setMatchDate(dateFormater);
-        
-        listMatch.add(newMatch);
-    }
+        matchList.add(newMatch);
+    }   
     
     public static void editMatch(List<Match> listMatch, JTextField nameA, JTextField nameB, JTextField date) {
         String playerA = nameA.getText();
         String playerB = nameB.getText();
-        String dateForm = date.getText();
-        if(playerA.isEmpty() || playerB.isEmpty() || dateForm.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Todos los campos deben estar llenos", "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        DateTimeFormatter dateFormatter= DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dateFormater= LocalDate.parse(dateForm, dateFormatter);
-        for(Match edit : listMatch) {
-            Player player1 = new Player();
-            player1.setName(playerA);
-            edit.setPlayerA(player1);
-            Player player2 = new Player();
-            player2.setName(playerB);
-            edit.setPlayerB(player2);
-            edit.setMatchDate(dateFormater);
-        }
-    }
-    
-    public static boolean deleteMatch(List<Match> listMatch, JTextField nameA, JTextField nameB, JTextField date) {
-        String playerA = nameA.getText();
-        String playerB = nameB.getText();
-        String dateForm = date.getText();
-        boolean matchDelete = false;
-        if(playerA.isEmpty() || playerB.isEmpty() || dateForm.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Todos los campos deben estar llenos", "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        DateTimeFormatter dateFormatter= DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dateFormater= LocalDate.parse(dateForm, dateFormatter);
+        LocalDate dateForm = StringBecomeLocalDate(date.getText());
+        Match editMatch = null;
         for(int i = 0; i < listMatch.size(); i++) {
-            Match match = listMatch.get(i);
-            String namePlayerA = match.getPlayerA().getName();
-            String namePlayerB = match.getPlayerB().getName();
-            LocalDate dateDelete = match.getMatchDate();
-            if(namePlayerA.equals(playerA) && namePlayerB.equals(playerB) && dateDelete.equals(dateFormater)) {
-                listMatch.remove(i);
-                matchDelete = true;
-                break;
+            Match m = listMatch.get(i);
+            String namePlayerA = m.getPlayerA().getName();
+            String namePlayerB = m.getPlayerB().getName();
+            LocalDate matchDate = m.getMatchDate();
+            if(namePlayerA.equals(playerA) && namePlayerB.equals(playerB) && matchDate.equals(dateForm)) {
+               editMatch = m;
+               break;
             }
         }
-        if(!matchDelete) {
-            JOptionPane.showMessageDialog(null, "No existe ese partido dentro de la lista de partidos que se jugaran", "ERROR", JOptionPane.ERROR_MESSAGE);
+        Player newPlayerA = null;
+        Player newPlayerB = null;
+        for(Player p : activeTournament.getParticipantsList()) {
+            if(p.getName().equals(playerA)) { newPlayerA = p;}
+            if(p.getName().equals(playerB)) { newPlayerB = p;}
         }
-        return matchDelete;
+        editMatch.setPlayerA(newPlayerA);
+        editMatch.setPlayerB(newPlayerB);
+        editMatch.setMatchDate(dateForm);
+        listMatch.add(editMatch);
     }
     
-    public static void changeNameTournament(JTextField newName, JTextField oldName){
-        String changeName = newName.getText();
-        if(changeName.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "El nombre a modificar no puede estar vacio si desea cambiar el nombre", "ERROR", JOptionPane.ERROR_MESSAGE);
-            return;
+    public static void deleteMatch(List<Match> listMatch, JTextField nameA, JTextField nameB, JTextField date) {
+        String playerAname = nameA.getText();
+        String playerBname = nameB.getText();
+        LocalDate dateForm = StringBecomeLocalDate(date.getText());
+        for(int i = 0; i < listMatch.size(); i++) {
+            Match m = listMatch.get(i);
+            String namePlayerA = m.getPlayerA().getName();
+            String namePlayerB = m.getPlayerB().getName();
+            LocalDate matchDate = m.getMatchDate();
+            if(namePlayerA.equals(playerAname) && namePlayerB.equals(playerBname) && matchDate.equals(dateForm)) {
+                listMatch.remove(m);
+                return;
+            }
         }
-        oldName.setText(changeName);
-    } 
+    }
+    
+    public static void changeNameTournament(JTextField newName){
+       String nameNew = newName.getText().trim();
+       String oldName = activeTournament.getTournamentName();
+       if(!oldName.equals(nameNew)) {
+           activeTournament.setTournamentName(nameNew);
+       } else { JOptionPane.showMessageDialog(null, "Los nombres son iguales por lo tanto no se ejecutara el cambio de nombre", "ERROR", JOptionPane.ERROR_MESSAGE);}
+    }    
 }
